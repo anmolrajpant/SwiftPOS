e3dddcf92f37c920133f95322a259320a058ff2d
anmolrajpant <anmolrajpant@hotmail.com>
2017-04-01 14:20:42 +0545

    catagory defined in inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,5 @@
 class Inventory < ApplicationRecord
 	validates :itemQuantity, :numericality => { :greater_than_or_equal_to => 0 }
 	validates :itemPrice, :numericality => { :greater_than_or_equal_to => 0} 
+	CATAGORIES = ['Glocery', 'Fruits', 'Frozen']
 end
